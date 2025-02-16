e790080e56feb0921c182032539b4ce99d05fe86
unknown <pachenkomih@gmail.com>
2025-02-16 23:24:43 +0200

    Application with login page where check the user data and access level

---
+++ Lesson_2/Task_1/Task_1/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_1.Models;
+
+namespace Task_1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Verify(LoginModel model)
+        {
+            var users = new List<LoginDataBaseModel>
+            {
+                new LoginDataBaseModel(1, "Danylo", "1111", true),
+                new LoginDataBaseModel(2, "Perdilo", "2222", false),
+                new LoginDataBaseModel(3, "Kukich", "3333", false),
+                new LoginDataBaseModel(4, "Admin", "4444", true),
+                new LoginDataBaseModel(5, "Soyjak", "5555", false)
+            };
+
+            if(String.IsNullOrEmpty(model.Username))
+            {
+                return View();
+            }    
+
+            var verifiedUser = users.FirstOrDefault(user =>
+                               user.Username == model.Username &&
+                               user.Password == model.Password);
+
+            if(verifiedUser != null)
+            {
+                return View("~/Views/Home/Privacy.cshtml", verifiedUser);
+            }
+            else
+            {
+                return View("Username or password invalid".Clone());
+            }
+
+                
+        }
+    }
+}
---
+++ Lesson_2/Task_1/Task_1/Models/LoginDataBaseModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_1.Models
+{
+    public class LoginDataBaseModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public LoginDataBaseModel(int id, string username, string password, bool isAdmin)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
+    }
+}
